4e862659f66953345f4cf90ae7c4bfabd79da91a
Linh Phạm Thị Mỹ <151426549+thien2403@users.noreply.github.com>
2024-05-10 05:57:25 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { View } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
+import {View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import MyStack from './routers/MyStack';
-import {MyContextControllerProvider} from './store';
+import { MyContextControllerProvider } from './store/Index';
 
 const App = () => {
   return (
     <View style={{flex: 1}}>
       <MyContextControllerProvider>
-        <PaperProvider>
           <NavigationContainer>
-            <MyStack />
+            <MyStack/>
           </NavigationContainer>
-        </PaperProvider>
       </MyContextControllerProvider>
     </View>
   );
