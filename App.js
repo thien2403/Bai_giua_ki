15d92a5d640a054cb033b207ee881dad793d5742
thien2403 <phamtranminhthien133@gmail.com>
2024-05-09 21:49:02 +0700

    Pham Thi My Linh

---
+++ App.js
@@ -0,0 +1,22 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
+import MyStack from './routers/MyStack';
+import {MyContextControllerProvider} from './store';
+
+const App = () => {
+  return (
+    <View style={{flex: 1}}>
+      <MyContextControllerProvider>
+        <PaperProvider>
+          <NavigationContainer>
+            <MyStack />
+          </NavigationContainer>
+        </PaperProvider>
+      </MyContextControllerProvider>
+    </View>
+  );
+};
+export default App;
